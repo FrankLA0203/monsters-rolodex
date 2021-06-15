c2076183ecb7176a843bf5adc417b4add2434b3c
FrankLA0203 <frankla0203@outlook.com>
2021-06-15 23:01:57 +0800

    Import data from json api and return state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello World'
-    };
+      monsters: []
+    }
+  };
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Hell' })}>Change Text</button>
-        </header>
+        {this.state.monsters.map(monster => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
       </div>
     );
   }
