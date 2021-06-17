de2c52930d981f0f0c8806f9cc81456ebd92dc5c
FrankLA0203 <frankla0203@outlook.com>
2021-06-17 12:46:19 +0800

    Import img media via API and css formating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   };
 
@@ -18,9 +20,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <input type='search' placeholder='search monsters' onChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
